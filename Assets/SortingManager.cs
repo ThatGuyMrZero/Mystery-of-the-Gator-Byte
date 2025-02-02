11b62dd9597c7a48893250b1db30136d2248ea98
chlimb <lavavamp@gmail.com>
2025-02-02 17:15:57 -0500

    sorting manager

---
+++ Assets/SortingManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SortingManager : MonoBehaviour
+{
+    public int booksNeededToWin = 3;
+    public Text winText;
+
+    private int leftZoneCount = 0;
+    private int rightZoneCount = 0;
+
+    void Start()
+    {
+        winText.gameObject.SetActive(false);
+    }
+
+    public void AddBookToZone(string zone)
+    {
+        if (zone == "LeftZone")
+            leftZoneCount++;
+        else if (zone == "RightZone")
+            rightZoneCount++;
+
+        CheckWinCondition();
+    }
+
+    void CheckWinCondition()
+    {
+        if (leftZoneCount >= booksNeededToWin || rightZoneCount >= booksNeededToWin)
+        {
+            winText.gameObject.SetActive(true);
+            Debug.Log("You did it!");
+        }
+    }
+}
