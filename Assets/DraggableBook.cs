94d18b40aa3a938e543c058bbe40f8a4a7108db3
chlimb <lavavamp@gmail.com>
2025-02-02 16:42:57 -0500

    testing draggability

--- Assets/DraggableBook.cs
+++ Assets/DraggableBook.cs
@@ -1,31 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DraggableBook : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DraggableBook : MonoBehaviour, IPointerDownHandler
 {
-    private Vector3 startPosition;
-    private bool isDragging = false;
-
-    public void OnBeginDrag(PointerEventData eventData)
+    public void OnPointerDown(PointerEventData eventData)
     {
-        startPosition = transform.position;
-        isDragging = true;
-        Debug.Log(gameObject.name + " started dragging");
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (isDragging)
-        {
-            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            newPosition.z = 0; // Keep it in 2D space
-            transform.position = newPosition;
-        }
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        isDragging = false;
-        Debug.Log(gameObject.name + " stopped dragging");
+        Debug.Log(gameObject.name + " was clicked! Dragging should work!");
     }
 }
