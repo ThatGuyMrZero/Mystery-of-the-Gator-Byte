c87878975786a383e2dbb9d078286ab24804f06a
chlimb <lavavamp@gmail.com>
2025-02-02 04:40:13 -0500

    clickable sprite

---
+++ Assets/ClickableSprite.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ClickableSprite : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Sprite clicked!");
+        GetComponent<SpriteRenderer>().color = Color.red; // Change color on click
+    }
+
+}
