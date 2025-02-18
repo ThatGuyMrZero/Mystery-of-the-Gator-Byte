148da723f04336c047631048b8760be4a0acf54d
sschlingmann04 <sschlingmann@ufl.edu>
2025-02-17 20:00:47 -0500

    make football clickable and added new scene

--- Assets/FootballScript.cs
+++ Assets/FootballScript.cs
@@ -5,18 +5,30 @@ public class FootballScript : MonoBehaviour
 {
     public GameController gameController;
     private SpriteRenderer spriteRenderer;
+    private Color originalColor;
     private bool minigameActive = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseEnter()
+    {
+        spriteRenderer.color = Color.yellow;    
+    }
+
+    private void OnMouseExit()
+    {
+        spriteRenderer.color = originalColor;
     }
 
     private void OnMouseDown()
--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public GameObject TextBeforeGame;
     public GameObject TextAfterGame;
+    public GameObject football;
+    private DialogueScript dialogueScript;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         TextBeforeGame.SetActive(true);
         TextAfterGame.SetActive(false);
+        football.SetActive(false);
         Debug.Log("Pregame text activated");
     }
 
@@ -23,11 +27,13 @@ public class GameController : MonoBehaviour
     {
         TextBeforeGame.SetActive(false);
         Debug.Log("Pregame text deactivated");
+        SceneManager.LoadScene("stadium_game");
     }
 
     public void EndMinigame()
     {
         TextAfterGame.SetActive(true);
         Debug.Log("Endgame text activated");
+        SceneManager.LoadScene("stadium");
     }
 }
