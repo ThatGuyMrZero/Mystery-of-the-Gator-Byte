e47e9f37a4edfab23929e36afad61f569742ab0b
kurtweber12 <kkw1995@hotmail.com>
2025-02-23 13:58:53 -0500

    Exported scene as package as a backup

--- Assets/OrderGenerator.cs
+++ Assets/OrderGenerator.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 public class OrderGenerator : MonoBehaviour
 {
 
+
     public TextMeshProUGUI orderText;
     public int maxOrders = 10;
     public int orderCount =0;
