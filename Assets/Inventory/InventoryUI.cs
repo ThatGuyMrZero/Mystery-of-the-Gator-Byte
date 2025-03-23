8f70900650621fc26b984a45f1b181376f6d2079
chlimb <lavavamp@gmail.com>
2025-03-23 15:06:27 -0400

    inventory manager implemented and library inventory item added.

--- Assets/Inventory/InventoryUI.cs
+++ Assets/Inventory/InventoryUI.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 
 public class InventoryUI : MonoBehaviour
@@ -6,48 +6,57 @@ public class InventoryUI : MonoBehaviour
     public GameObject inventoryPanel;
     public Transform itemHolder;
 
-    // Assign prefabs in Inspector
+    // Item prefab references
     public GameObject libraryKeyPrefab;
 
+
     private Dictionary<string, GameObject> itemPrefabLookup;
+
+    private bool inventoryVisible = false;
 
     void Start()
     {
         itemPrefabLookup = new Dictionary<string, GameObject>
         {
             { "Library Key", libraryKeyPrefab },
+
         };
+
+        inventoryPanel.SetActive(false);
     }
 
     public void ToggleInventory()
     {
-        bool isActive = inventoryPanel.activeSelf;
-        inventoryPanel.SetActive(!isActive);
+        inventoryVisible = !inventoryVisible;
+        inventoryPanel.SetActive(inventoryVisible);
 
-        if (!isActive)
+        if (inventoryVisible)
         {
-            ShowItems();
+            RefreshInventoryUI();
         }
     }
 
-    void ShowItems()
+    void RefreshInventoryUI()
     {
+        // 🔄 Clear all previously spawned sprite children
         foreach (Transform child in itemHolder)
         {
             Destroy(child.gameObject);
         }
 
+        // 🆕 Instantiate sprites from inventory
+        int i = 0;
         foreach (string item in InventoryManager.Instance.items)
         {
             if (itemPrefabLookup.ContainsKey(item))
             {
-                GameObject itemPrefab = itemPrefabLookup[item];
-                GameObject newItem = Instantiate(itemPrefab, itemHolder);
-                newItem.transform.localPosition = new Vector3(0, -itemHolder.childCount * 1.5f, 0);
+                GameObject newItem = Instantiate(itemPrefabLookup[item], itemHolder);
+                newItem.transform.localPosition = new Vector3(0, -i * 1.5f, 0);
+                i++;
             }
             else
             {
-                Debug.LogWarning($"No sprite prefab found for item: {item}");
+                Debug.LogWarning($"No prefab found for item '{item}'");
             }
         }
     }
--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -22,7 +22,15 @@ public class InventoryManager : MonoBehaviour
 
     public void AddItem(string itemName)
     {
-        items.Add(itemName);
-        Debug.Log($"Item added: {itemName}");
+        if (!items.Contains(itemName))
+        {
+            items.Add(itemName);
+            Debug.Log($"Item added: {itemName}");
+        }
+        else
+        {
+            Debug.Log($"Item '{itemName}' already in inventory!");
+        }
     }
+
 }
