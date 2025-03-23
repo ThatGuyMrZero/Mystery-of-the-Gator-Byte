398ee47e85370fbed8e0f213a6d28512916d6776
chlimb <lavavamp@gmail.com>
2025-03-23 14:25:07 -0400

    inventory manager

---
+++ Assets/InventoryManager.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+
+    public List<string> items = new List<string>();
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Persist between scenes
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddItem(string itemName)
+    {
+        items.Add(itemName);
+        Debug.Log($"Item added: {itemName}");
+    }
+}
