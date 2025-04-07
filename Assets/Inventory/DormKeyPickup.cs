88eaedb0668069526304f70da7d0b3bee9b5aa56
chlimb <lavavamp@gmail.com>
2025-04-07 10:19:55 -0400

    key goes to final scene

---
+++ Assets/Inventory/DormKeyPickup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DormKeyPickup : MonoBehaviour
+{
+    public string sceneToLoad = "FinalScene"; // Set this in the Inspector
+
+    void OnMouseDown()
+    {
+        Debug.Log("🔑 Key clicked! Loading final scene...");
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
