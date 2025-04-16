b0c61eb85ededae7384b7e1cbb3da93d991925c5
ThatGuyMrZero <zeromichael@yahoo.com>
2025-04-16 02:30:56 -0400

    added loading screen

---
+++ Assets/LoadingScreenClassroomChanger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenClassroomChanger : MonoBehaviour
+{
+    public string SceneName;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
